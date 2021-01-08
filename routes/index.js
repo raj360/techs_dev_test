b3a0d84908e11d0875c49f8afee0f7043df1cc8a
Raymond <rayjxclusive@gmail.com>
2021-01-08 21:38:04 +0300

    count_per_recipe

--- routes/index.js
+++ routes/index.js
@@ -17,10 +17,29 @@ const recipes = data.map(recipe=> recipe.recipe) //generate array of with names
                  return  array.indexOf(name) === index //if exists return -1, hence don't include
       })
 
-  res.json({unique_recipe_count:recipes.length})
+      res.json({unique_recipe_count:recipes.length})
 })
 
+router.get('/count_per_recipe',(req, res)=>{
 
+const recipesAndCount = data.map(recipe=> recipe.recipe ) //generate array of with names
+                       .reduce((array, currentRecipe) =>{ //iterating over the recipes
+              
+         let  recipe =   array.find(elem => elem.recipe === currentRecipe) //check for occurrance in my new array
+
+           if(recipe === undefined) {
+           array.push({recipe:currentRecipe,count:1});
+          }else{
+        let recipeAtIndex = array.findIndex(element=> element.recipe == currentRecipe );
+       array[recipeAtIndex] = {...recipe,count:recipe.count+1};
+
+        }
+       return array
+
+}, []);
+
+   res.json({count_per_recipe:recipesAndCount})
+})
 
 
 
