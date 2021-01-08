616d852c257352e4de5b92d53be1a0f746978bef
Raymond <rayjxclusive@gmail.com>
2021-01-08 23:25:05 +0300

    busiest_postcode

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,11 @@ const data = require('../data.json')
 
 
 
+const  getKeyByValue = (object, value) =>  {
+  return Object.keys(object).find(key => object[key] === value);
+}
+
+
 router.get('/',(req,res)=>{
  res.json({message:'Dev test api...'});
 })
@@ -25,21 +30,49 @@ router.get('/count_per_recipe',(req, res)=>{
 const recipesAndCount = data.map(recipe=> recipe.recipe ) //generate array of with names
                        .reduce((array, currentRecipe) =>{ //iterating over the recipes
               
-         let  recipe =   array.find(elem => elem.recipe === currentRecipe) //check for occurrance in my new array
+             let  recipe =   array.find(elem => elem.recipe === currentRecipe) //check for occurrance in my new array
 
-           if(recipe === undefined) {
-           array.push({recipe:currentRecipe,count:1});
+          if(recipe === undefined) {
+                 array.push({recipe:currentRecipe,count:1});
           }else{
-        let recipeAtIndex = array.findIndex(element=> element.recipe == currentRecipe );
-       array[recipeAtIndex] = {...recipe,count:recipe.count+1};
+
+        let recipeAtIndex = array.findIndex(element=> element.recipe == currentRecipe ); //find recipe index
+         array[recipeAtIndex] = {...recipe,count:recipe.count+1}; //update recipe count
 
         }
        return array
 
 }, []);
 
+
    res.json({count_per_recipe:recipesAndCount})
 })
+
+
+
+router.get('/busiest_postcode',(req,res)=>{
+
+  let postCounts = {};
+
+  //if this data is too much we can use promises 
+  // or async awaits to prevent the lover code from
+  //running before this
+ data.map(element=> element.postcode)
+                              .forEach((postCode)=>{
+                                    postCounts[postCode] = (postCounts[postCode ] || 0) + 1
+                                     })
+
+     let counts = Object.values(postCounts) ;                                  
+  
+     let max =  Math.max(...counts) 
+   
+     let postcode  = getKeyByValue(postCounts,max)
+
+  res.json({postcode,delivery_count:max})
+})
+
+
+
 
 
 
